a67ec311cb700cc0cd6839b25fea2610330f0ffc
leodrone <nfwleo98@163.com>
2025-07-09 16:19:22 +0800

    fix:修改

--- packages/web/src/modules/FunctionBarAndLinkmanList/CreateGroup.tsx
+++ packages/web/src/modules/FunctionBarAndLinkmanList/CreateGroup.tsx
@@ -44,8 +44,8 @@ function CreateGroup(props: CreateGroupProps) {
                     value={priGroup} // ...强制选择框的值与 state 相匹配...
                     onChange={e => updatePriGroup(e.target.value)} // ... 并在每次改变（change）时更新 state
                     >
-                    <option :value="false">公开</option>
-                    <option :value="true">私有</option>
+                    <option value={false}>公开</option>
+                    <option value={true}>私有</option>
                 </select>
      
                 <button
